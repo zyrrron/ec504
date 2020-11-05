11845057856027331208d4f2313789368da446a4
brower <brower@bu.edu>
2020-11-04 22:38:57 -0500

    MidTerm_codes

---
+++ MidTerm_codes/tree.h
@@ -0,0 +1,26 @@
+
+
+/* START: fig4_16.txt */
+        #ifndef _Tree_H
+        #define _Tree_H
+
+        typedef int ElementType;
+	typedef int HeightType;
+
+        struct TreeNode;
+        typedef struct TreeNode *Position;
+        typedef struct TreeNode *SearchTree;
+
+        SearchTree MakeEmpty( SearchTree T );
+	HeightType CalculateHeight(SearchTree T);
+	HeightType Height(SearchTree T);
+        Position Find( ElementType X, SearchTree T );
+        Position FindMin( SearchTree T );
+        Position FindMax( SearchTree T );
+        SearchTree Insert( ElementType X, SearchTree T );
+        SearchTree Delete( ElementType X, SearchTree T );
+        ElementType Retrieve( Position P );
+
+        #endif  /* _Tree_H */
+
+/* END */
